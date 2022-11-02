1284749c17c3a200508f67db39b0640a5278f6eb
Tim Howe <rrritalin@Tims-Air.lan>
2022-11-02 15:49:33 +0000

    This update adds the python file manual_rps.py, which defines two functions: get_computer_choice and get_user_choice. The former determines the computer’s move from a random choice of rock, paper or scissors. The latter function waits for user input of the single characters ‘r’ ,’p’, or ’s’ and returns the appropriate full string (rock, paper, or scissors).

---
+++ manual_rps.py
@@ -0,0 +1,43 @@
+# Manual-only Rock-Paper-Scissors game
+
+
+# import packages
+import numpy as np
+import random
+# import cv2
+# from keras.models import load_model
+
+# Define the functions 
+
+def get_computer_choice():
+    # Define list of possible moves
+    move_list = ["rock", "paper", "scissors"]
+    # Choose a random word from the list of possible words
+    computer_choice = random.choice(move_list)
+    return(computer_choice)
+
+def get_user_choice():
+    move_list = {
+    "r": "rock",
+    "p": "paper",
+    "s": "scissors"
+    }
+    while True:
+        # Request input from user of a single letter
+        user_choice=input("please enter a move (r,p,s = rock, paper, scissors")
+        #checks that input is alphabetical AND of length 1. Escapes if both conditions met
+        if user_choice in move_list.keys():
+            print('thanks!')
+            user_guess=move_list[user_choice]
+            return(user_guess)
+        else:
+            print("Invalid letter. Please, enter a single alphabetical character.")
+
+# test the code
+computer_choice=get_computer_choice()
+print("computer's move is:")
+print(computer_choice)
+
+user_choice=get_user_choice()
+print("user's move is:")
+print(user_choice)
